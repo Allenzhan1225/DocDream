0715b6dd57cf493bdd16f75c45517573fbb9d342
Allenzhan1225 <1042748558@qq.com>
2017-10-09 10:40:35 +0800

    Initial Commit

---
+++ DocDream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DocDream
+//
+//  Created by YDWY on 2017/10/9.
+//  Copyright © 2017年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DocDream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DocDream
+//
+//  Created by YDWY on 2017/10/9.
+//  Copyright © 2017年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
